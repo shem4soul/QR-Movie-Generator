29e88927e20515a14251ecf1e8734c9a9af56ab5
shem4soul@gmail.com <shem4soul@gmail.com>
2025-06-14 01:19:04 +0100

    refactor: remove commented-out code and update Movie import from Prisma client

--- dist/main.js
+++ dist/main.js
@@ -26,17 +26,3 @@ async function bootstrap() {
     console.log(`📚 Swagger docs available at http://localhost:${PORT}/api`);
 }
 bootstrap();
-// import { NestFactory } from "@nestjs/core";
-// import { AppModule } from "./app.module";
-// import { NestExpressApplication } from "@nestjs/platform-express";
-// import { join } from "path";
-// async function bootstrap() {
-//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-//   app.useStaticAssets(join(__dirname, "..", "public"));
-//   app.setBaseViewsDir(join(__dirname, "..", "views"));
-//   app.setViewEngine("ejs");
-//   const PORT = process.env.PORT || 3000;
-//   await app.listen(PORT);
-//   console.log(`Server running on http://localhost:${PORT}`);
-// }
-// bootstrap();
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Post } from "@nestjs/common";
 import { MoviesService } from "./movies.service";
-import { Movie } from "../../generated/prisma";
+import { Movie } from "@prisma/client";
 
 @Controller("movies")
 export class MoviesController {
